66bb6269df3dd2955fb4f5c57b4d9069f802e964
omkar udawant <udawantomkar825@gmail.com>
2026-08-17 11:01:52 +0530

    Add files via upload

---
+++ dsa/Arrays/Left Rotate array by one place/code.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotateByOne(vector<int>& nums)
+{
+    int temp = nums[0];
+
+    for(int i = 1; i < nums.size(); i++)
+    {
+        nums[i - 1] = nums[i];
+    }
+
+    nums[nums.size() - 1] = temp;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5};
+
+    rotateByOne(nums);
+
+    cout << "After rotation: ";
+
+    for(int x : nums)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
